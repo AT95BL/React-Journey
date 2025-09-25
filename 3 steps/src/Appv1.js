349f30485268cde99ec8c2b73e94f4146dea2513
Andrej Trožić <andrejtrozic95@gmail.com>
2025-09-25 12:17:28 +0200

    update
    "children" props added in both files

--- 3 steps/src/Appv1.js
+++ 3 steps/src/Appv1.js
@@ -7,7 +7,7 @@ const messages = [
   "Invest your new income 🤑",
 ];
 
-export default function App(){
+function Appv1(){
 
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(true);
@@ -43,28 +43,11 @@ export default function App(){
           <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
         </div>
 
-      <StepMessage step={step}>
-        {messages[step - 1]}
-        <div className='buttons'>
-          <Button
-          bgColor={"#e7e7e7"}
-          textColor={"#333"}
-          onClick={() => alert(`Learn how to ${messages[step - 1]}`)}
-          >Learn how</Button>
-        </div>
-      </StepMessage>
+      <p className="message">Step {step}: {messages[step-1]}</p>
 
       <div className="buttons">
-        <Button 
-            bgColor={"#7950f2"} 
-            textColor={"#fff"} 
-            onClick = {callPrevious} 
-        ><span>👈</span>Previous</Button>
-        <Button 
-            bgColor={"#7950f2"} 
-            textColor={"#fff"} 
-            onClick = {callNext} 
-        >Next<span>👉</span></Button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick = {callPrevious}>Previous</button>
+        <button style={{backgroundColor: "#7950f2", color: "#fff"}} onClick = {callNext}>Next</button>
       </div>
      </div>)
     }
@@ -72,15 +55,4 @@ export default function App(){
   );
 }
 
-function Button({textColor, bgColor, onClick, children}) {
-  return <button style={{backgroundColor: bgColor, color: textColor}} onClick={onClick}>
-    {children}
-  </button>;
-}
-
-function StepMessage({step, children}) {
-  return <p className="message">
-    <h3>Step {step}</h3>
-    {children}
-  </p>;
-}
+export default Appv1;
